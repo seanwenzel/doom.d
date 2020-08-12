f125fb3edd7b20f3bf00f2292046f31a986ae0e5
Sean Wenzel <sean.d.wenzel@jpl.nasa.gov>
2020-08-11 17:46:04 -0700

    Remove avy

--- config.el
+++ config.el
@@ -63,11 +63,4 @@
   :config
   (map! :leader :desc "Snippet" "i s" #'ivy-yasnippet))
 
-(use-package avy
-  :config
-  (map! :leader
-        "j j" #'evil-avy-goto-char-timer
-        "j w" #'evil-avy-goto-word-1
-        "j l" #'evil-avy-goto-line))
-
 (map! :leader :desc "M-x" "SPC" #'counsel-M-x)
--- init.el
+++ init.el
@@ -52,7 +52,7 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       ;;(format +onsave)  ; automated prettiness
+       (format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
@@ -92,7 +92,7 @@
        lookup              ; navigate your code and its documentation
        lsp
        ;;macos             ; MacOS-specific commands
-       magit             ; a git porcelain for Emacs
+       (magit +forge)      ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
