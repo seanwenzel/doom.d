e73d3fac0b17f5d0e3324c644fc7f4b2823c961d
Sean Wenzel <sean.d.wenzel@jpl.nasa.gov>
2021-03-29 15:12:28 -0700

    better macos keybindings

--- init.el
+++ init.el
@@ -91,7 +91,7 @@
        ;;gist              ; interacting with github gists
        (lookup +docsets)   ; navigate your code and its documentation
        ;; lsp
-       ;;macos             ; MacOS-specific commands
+       macos             ; MacOS-specific commands
        (magit +forge)      ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
